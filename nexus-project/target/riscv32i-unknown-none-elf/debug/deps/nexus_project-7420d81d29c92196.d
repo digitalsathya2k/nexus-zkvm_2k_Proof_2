8809e6d5f67449d9126b73dfeb9af7ec7ed82dd1
digitalsathya2k n <digitalsathya2k@gmail.com>
2024-07-22 04:31:38 +0000

    Results_2nd Proof

---
+++ nexus-project/target/riscv32i-unknown-none-elf/debug/deps/nexus_project-7420d81d29c92196.d
@@ -0,0 +1,5 @@
+/workspace/nexus-zkvm_2k_Proof_2/nexus-project/target/riscv32i-unknown-none-elf/debug/deps/nexus_project-7420d81d29c92196: src/main.rs
+
+/workspace/nexus-zkvm_2k_Proof_2/nexus-project/target/riscv32i-unknown-none-elf/debug/deps/nexus_project-7420d81d29c92196.d: src/main.rs
+
+src/main.rs:
---
+++ nexus-project/target/riscv32i-unknown-none-elf/debug/nexus-project.d
@@ -0,0 +1,1 @@
+/workspace/nexus-zkvm_2k_Proof_2/nexus-project/target/riscv32i-unknown-none-elf/debug/nexus-project: /workspace/nexus-zkvm_2k_Proof_2/nexus-project/src/main.rs
